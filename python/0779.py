9c3b6b88f14b0549c2b24a19f2dbbe33341634bd
MylesByrne <mylesbyrne01@gmail.com>
2023-10-25 12:42:29 -0400

    Create 0779.py

---
+++ python/0779.py
@@ -0,0 +1,10 @@
+class Solution:
+    def kthGrammar(self, n: int, k: int) -> int:
+        # 0
+        if n == 1:
+            return 0
+        midpoint = 2 ** (n-2)
+        if k <= midpoint:
+            return self.kthGrammar(n-1, k)
+        else:
+            return 1 - self.kthGrammar(n-1, k - midpoint)
